6ff568b9cc9d123e3898842b5d9dc4681163ecdf
Adriane Justine Tan <eidoriantan@gmail.com>
2020-06-12 23:44:40 +0800

    style: use double quotes to use line breaks

--- chat/server.php
+++ chat/server.php
@@ -12,7 +12,7 @@ $ws_server = new WsServer($chat);
 $http_server = new HttpServer($ws_server);
 
 $server = IoServer::factory($http_server, 8000);
-echo 'Server is running on port 8000\r\n';
+echo "Server is running on port 8000\r\n";
 $server->run();
 
 ?>
