89815b0929775c3fcc8c41b5030849fde48b3a49
Adriane Justine Tan <eidoriantan@gmail.com>
2020-06-14 11:15:16 +0800

    refactor: use `config.php` file for server configuration

--- chat/server.php
+++ chat/server.php
@@ -6,13 +6,15 @@ use Ratchet\WebSocket\WsServer;
 use ChatApp\App\Chat;
 
 require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/config.php';
 
 $chat = new Chat();
 $ws_server = new WsServer($chat);
 $http_server = new HttpServer($ws_server);
 
-$server = IoServer::factory($http_server, 8000);
-echo "Server is running on port 8000\r\n";
+$port = $config['port'];
+$server = IoServer::factory($http_server, $port);
+echo "Server is running on port $port\r\n";
 $server->run();
 
 ?>
