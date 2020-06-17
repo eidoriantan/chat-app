bae17a1a522800e7edc46c3f19ad06092a8cc1fe
Adriane Justine Tan <eidoriantan@gmail.com>
2020-06-17 12:02:05 +0800

    build(server): use Microsoft Azure servers

--- chat/config.php
+++ chat/config.php
@@ -1,7 +1,11 @@
 <?php
 
 $config = [
-  'port' => isset($_SERVER['PORT']) ? $_SERVER['PORT'] : 8000
+  'port' => isset($_SERVER['PORT']) ? $_SERVER['PORT'] : 8000,
+  'ssl' => [
+    'certificate' => '/home/eidoriantan/0000_cert.pem',
+    'key' => '/home/eidoriantan/private.key'
+  ]
 ];
 
 ?>
